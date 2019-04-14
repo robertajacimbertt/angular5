29f23b0ae1d329827f8670b151488cfaef35f654
Roberta Jacimbertt de Jesus <robertaj@br.ibm.com>
2019-04-14 11:10:27 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// IMPORTS
 import { Component } from '@angular/core';
 
+// COMPONENT DECORATOR
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', // SELETOR
+  templateUrl: './app.component.html', //template html
+  styleUrls: ['./app.component.scss'] // estilos
 })
+
+// EXPORT, LOGIC
 export class AppComponent {
   title = 'ng5';
 }
